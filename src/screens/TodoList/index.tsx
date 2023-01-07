3c72ea5482977832ab2d270e63c70fa773c2df0d
Diogo Sant'Anna <diogosantanna08@gmail.com>
2023-01-07 15:34:57 -0300

    feat: starts implementing the TodoList screen

--- src/screens/TodoList/index.tsx
+++ src/screens/TodoList/index.tsx
@@ -1,19 +1,48 @@
-import { useNavigation } from '@react-navigation/native';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback, View } from 'react-native';
+import Toast from 'react-native-toast-message';
+
+import { Spinner } from '@components/Spinner';
+import { TodoForm } from '@components/TodoForm';
 
 export function TodoList() {
-  const { navigate } = useNavigation();
+  const [loading, setLoading] = useState(false);
+
+  // Lógica para carregar os dados aqui
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  const handleAddToDo = async (todo: string) => {
+    setLoading(true);
+    await new Promise<void>((resolve) =>
+      setTimeout(() => {
+        Toast.show({
+          type: 'success',
+          text1: todo,
+          position: 'bottom',
+          visibilityTime: 2000,
+          autoHide: true,
+        });
+        resolve();
+      }, 3000),
+    );
+    setLoading(false);
+  };
+
   return (
-    <View className="flex-1 flex-col bg-gray-900 items-center justify-center">
-      <Text className="text-gray-100 absolute top-12 text-3xl">Todo List</Text>
-
-      <TouchableOpacity onPress={() => navigate('singIn')}>
-        <Text className="text-green-400 m-2">Sing In</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => navigate('singUp')}>
-        <Text className="text-green-400 m-2">Sing Up</Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View className="w-screen h-full bg-gray-800">
+        <View className="items-center justify-center mx-auto my-5 h-8">
+          {loading && <Spinner duration={0.7} />}
+        </View>
+        <KeyboardAvoidingView>
+          <TodoForm addToDo={handleAddToDo} />
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
